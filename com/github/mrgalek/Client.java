bbc7f867967bf3c8421eb9c141e1b13cd9805846
MrGalek <damgalczynski@gmail.com>
2020-02-24 20:18:35 +0100

    Add simple UI for client and staff Classes have been completed

--- com/github/mrgalek/Client.java
+++ com/github/mrgalek/Client.java
@@ -3,10 +3,22 @@ package com.github.mrgalek;
 public class Client extends Person
 {
     private double accountStand;
+    private ClientTransaction clientTransaction;
+
+    Client()
+    {
+        super();
+        clientTransaction = new ClientTransaction();
+    }
 
     public double getAccountStand()
     {
         return accountStand;
+    }
+
+    public void setAccountStand(double accountStand)
+    {
+        this.accountStand = accountStand;
     }
 
     public void addMoney(double moneyToBeAdded)
@@ -17,5 +29,10 @@ public class Client extends Person
     public void deductMoney(double moneyToBeDeducted)
     {
         accountStand -=moneyToBeDeducted;
+    }
+
+    public ClientTransaction getClientTransaction()
+    {
+        return clientTransaction;
     }
 }
--- com/github/mrgalek/StaffTransaction.java
+++ com/github/mrgalek/StaffTransaction.java
@@ -3,13 +3,19 @@ package com.github.mrgalek;
 public class StaffTransaction extends Transaction
 {
     @Override
-    public boolean transferMoney()
+    public boolean transferMoney(Client fromClinet, Client toClient)
     {
         return false;
     }
 
     public boolean paymentOnAccount()
     {
-
+        return true;
     }
+
+    public boolean withdrawalFromAccount()
+    {
+        return true;
+    }
+
 }
